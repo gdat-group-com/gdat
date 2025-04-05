7a1b83ab91eb6dfc94cc854041b1dda126e9d139
GDAT GROUP <gdatgroup.vn@gmail.com>
2025-04-06 00:33:38 +0700

    Update GDATToken.sol

--- contracts/GDATToken.sol
+++ contracts/GDATToken.sol
@@ -6,9 +6,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GDATToken is ERC20, Ownable {
     uint256 public constant MAX_SUPPLY = 5000000 * 10**18;
-    uint256 public constant ICO_START = 1711929600; // 2025-04-01 00:00:00 UTC
-    uint256 public constant ICO_END = 1714617599; // 2025-06-30 23:59:59 UTC
-    uint256 public tokenPrice = 0.01 ether; // Example token price
+    uint256 public constant STO_START = 1711929600; // 2025-04-01 00:00:00 UTC
+    uint256 public constant STO_END = 1714617599; // 2025-06-30 23:59:59 UTC
+    
+    uint256 public constant APRIL_PRICE = 0.5 ether; // Example price in wei
+    uint256 public constant MAY_PRICE = 0.75 ether; // Example price in wei
+    uint256 public constant JUNE_PRICE = 1 ether; // Example price in wei
     
     mapping(address => bool) public whitelist;
     address public fundReceiver;
@@ -29,12 +32,25 @@ contract GDATToken is ERC20, Ownable {
         whitelist[_address] = false;
     }
 
+    function getCurrentPrice() public view returns (uint256) {
+        if (block.timestamp >= STO_START && block.timestamp < STO_START + 30 days) {
+            return APRIL_PRICE;
+        } else if (block.timestamp >= STO_START + 30 days && block.timestamp < STO_START + 60 days) {
+            return MAY_PRICE;
+        } else if (block.timestamp >= STO_START + 60 days && block.timestamp <= STO_END) {
+            return JUNE_PRICE;
+        } else {
+            revert("STO is not active");
+        }
+    }
+
     function buyTokens() external payable {
-        require(block.timestamp >= ICO_START && block.timestamp <= ICO_END, "ICO is not active");
+        require(block.timestamp >= STO_START && block.timestamp <= STO_END, "STO is not active");
         require(whitelist[msg.sender], "You are not whitelisted");
         require(msg.value > 0, "Send ETH to buy tokens");
 
-        uint256 tokensToBuy = msg.value / tokenPrice;
+        uint256 currentPrice = getCurrentPrice();
+        uint256 tokensToBuy = msg.value / currentPrice;
         require(balanceOf(owner()) >= tokensToBuy, "Not enough tokens available");
         require(totalSupply() + tokensToBuy <= MAX_SUPPLY, "Exceeds max supply");
 
