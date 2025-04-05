d991512854b3c41ea7f099edc26ab8cab7d8413c
GDAT GROUP <gdatgroup.vn@gmail.com>
2025-04-06 00:36:33 +0700

    Update GDATToken.sol

--- contracts/GDATToken.sol
+++ contracts/GDATToken.sol
@@ -8,20 +8,23 @@ contract GDATToken is ERC20, Ownable {
     uint256 public constant MAX_SUPPLY = 5000000 * 10**18;
     uint256 public constant STO_START = 1711929600; // 2025-04-01 00:00:00 UTC
     uint256 public constant STO_END = 1714617599; // 2025-06-30 23:59:59 UTC
+
+    uint256 public constant APRIL_PRICE_USD = 0.5 * 10**18; // 0.5 USD
+    uint256 public constant MAY_PRICE_USD = 0.75 * 10**18; // 0.75 USD
+    uint256 public constant JUNE_PRICE_USD = 1 * 10**18; // 1 USD
     
-    uint256 public constant APRIL_PRICE = 0.5 ether; // Example price in wei
-    uint256 public constant MAY_PRICE = 0.75 ether; // Example price in wei
-    uint256 public constant JUNE_PRICE = 1 ether; // Example price in wei
-    
+    uint256 public ethUsdRate; // ETH/USD rate in wei
+
     mapping(address => bool) public whitelist;
     address public fundReceiver;
 
     event TokensPurchased(address indexed buyer, uint256 amount);
     event FundsWithdrawn(address indexed receiver, uint256 amount);
 
-    constructor(address _fundReceiver) ERC20("GDAT Token", "GDAT") {
+    constructor(address _fundReceiver, uint256 _ethUsdRate) ERC20("GDAT Token", "GDAT") {
         _mint(msg.sender, MAX_SUPPLY);
         fundReceiver = _fundReceiver;
+        ethUsdRate = _ethUsdRate;
     }
 
     function addToWhitelist(address _address) external onlyOwner {
@@ -32,13 +35,17 @@ contract GDATToken is ERC20, Ownable {
         whitelist[_address] = false;
     }
 
+    function updateEthUsdRate(uint256 _ethUsdRate) external onlyOwner {
+        ethUsdRate = _ethUsdRate;
+    }
+
     function getCurrentPrice() public view returns (uint256) {
         if (block.timestamp >= STO_START && block.timestamp < STO_START + 30 days) {
-            return APRIL_PRICE;
+            return (APRIL_PRICE_USD * 10**18) / ethUsdRate; // Convert USD to wei
         } else if (block.timestamp >= STO_START + 30 days && block.timestamp < STO_START + 60 days) {
-            return MAY_PRICE;
+            return (MAY_PRICE_USD * 10**18) / ethUsdRate; // Convert USD to wei
         } else if (block.timestamp >= STO_START + 60 days && block.timestamp <= STO_END) {
-            return JUNE_PRICE;
+            return (JUNE_PRICE_USD * 10**18) / ethUsdRate; // Convert USD to wei
         } else {
             revert("STO is not active");
         }
@@ -50,7 +57,7 @@ contract GDATToken is ERC20, Ownable {
         require(msg.value > 0, "Send ETH to buy tokens");
 
         uint256 currentPrice = getCurrentPrice();
-        uint256 tokensToBuy = msg.value / currentPrice;
+        uint256 tokensToBuy = msg.value * ethUsdRate / currentPrice;
         require(balanceOf(owner()) >= tokensToBuy, "Not enough tokens available");
         require(totalSupply() + tokensToBuy <= MAX_SUPPLY, "Exceeds max supply");
 
